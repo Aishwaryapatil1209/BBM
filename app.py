376fa8694f33598d0e1f854cb96a561b628fda98
Aishwarya Patil <patilaishwarya003@gmail.com>
2025-07-20 13:43:53 +0530

    huh

--- app.py
+++ app.py
@@ -95,10 +95,5 @@ def send_email():
     except Exception as e:
         return f"<h3 style='color:red;'>Something went wrong: {str(e)}</h3>"
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
 if __name__ == "__main__":
-    import os
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-
+    app.run(debug=True)
